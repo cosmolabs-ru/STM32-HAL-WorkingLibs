0367df62fcc46d0351bd1c90dae5a13bde74e09b
gpsgarage-ru <enqie95@gmail.com>
2019-03-23 11:28:15 +0300

    Add files via upload

---
+++ lcd1602_i2c/lcdi2c.h
@@ -0,0 +1,27 @@
+/*
+ * lcdi2c.h
+ *
+ *  Created on: 23 мар. 2019 г.
+ *      Author: enq
+ */
+
+#ifndef LCDI2C_H_
+#define LCDI2C_H_
+
+#define LCD_ADDR (0x27 << 1)
+
+#define PIN_RS    (1 << 0)
+#define PIN_EN    (1 << 2)
+#define BACKLIGHT (1 << 3)
+
+#define LCD_DELAY_MS 5
+
+void I2C_Scan();
+HAL_StatusTypeDef LCD_SendInternal(uint8_t lcd_addr, uint8_t data, uint8_t flags);
+void LCD_SendCommand(uint8_t lcd_addr, uint8_t cmd);
+void LCD_SendData(uint8_t lcd_addr, uint8_t data);
+void LCD_Init(uint8_t lcd_addr);
+void LCD_SendString(uint8_t lcd_addr, char *str);
+
+
+#endif /* LCDI2C_H_ */
